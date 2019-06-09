c3ddb3dd8bce74d1d18488448887450e0b996ab5
Moza <27530016+AzMoza@users.noreply.github.com>
2019-06-10 00:48:56 +0100

    Implemented basic JS aqueduct model generator.

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +19,43 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.aquModel', async function () {
+		if(!vscode.workspace.workspaceFolders) {
+			return vscode.window.showErrorMessage("Please open a directory before running this command");
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const modelName = await vscode.window.showInputBox({
+			prompt: "Model Name"
+		});
+
+		if(!modelName) return;
+
+		let property = await vscode.window.showInputBox({
+			prompt: "Property Name (Type \"done\" when finished)"
+		});
+
+		const properties = [];
+
+		while(property !== 'done') {
+			properties.push(property);
+			property = await vscode.window.showInputBox({
+				prompt: "Property Name (Type \"done\" when finished)"
+			});
+		}
+		const decleration = properties.map(prop => `\t\t@Column(nullable: false) String ${prop};\n`);
+		const classContent = `class ${modelName} extends ManagedObject<_${modelName}> implements _${modelName} {}\n\nclass _${modelName} {\n\t${decleration}\n}`;
+		
+		const folderPath = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
+
+		// TODO: Get this to work currently getting a path error which i believe is something to do with [folderPath]
+		fs.writeFile(path.join(folderPath, "model.dart"), classContent, err => {
+			if (err) {
+			  return vscode.window.showErrorMessage(
+				"Failed to create model file!"
+			  );
+			}
+			vscode.window.showInformationMessage("Created model file");
+		  });
 	});
 
 	context.subscriptions.push(disposable);
